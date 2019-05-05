1eaeced3bd9206219d1c153423ba0939ec15b7de
louisenorris <louisenorris73@yahoo.co.uk>
2019-05-06 00:15:21 +0100

    Completed Bachelor Nested Iteration lab

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,67 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+	winner = nil
+	data.each do |seasons, info|
+		if seasons == season
+	  	info.each do |contestant|
+	  		if contestant["status"] == "Winner"
+	  			winner = contestant["name"].split[0]
+	  		end
+	  	end
+	  	end
+	end
+	winner
 end
+
 
 def get_contestant_name(data, occupation)
-  # code here
+	contestant_name = nil
+	data.each do |seasons, info|
+		info.each do |contestant|
+			if contestant["occupation"] == occupation
+				contestant_name = contestant["name"]
+			end
+		end
+	end
+	contestant_name
 end
+
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+	counter = 0
+	data.each do |seasons, info|
+		info.each do |contestant|
+  		  if contestant["hometown"] == hometown
+  		  	counter += 1
+  		  end
+  		end
+  	end
+
+counter
 end
 
+
 def get_occupation(data, hometown)
-  # code here
+	data.collect do |seasons, info|
+		info.find do |contestant| 
+			if contestant["hometown"] == hometown
+		  	return contestant["occupation"]
+		  	end
+		end
+	end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+age = 0
+count = 0
+  data[season].each do |contestant|
+    contestant.each do |key, value|
+      if key == "age"
+        count += 1
+        age += value.to_f
+      end
+    end
+  end
+
+   average_age = (age/count).round
+return average_age
 end
